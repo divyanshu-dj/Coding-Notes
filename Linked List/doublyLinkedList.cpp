1a567aab16f9ca410b47b16916f917f2901b4ce1
Divyanshu Kumar Jha <116970264+divyanshu-dj@users.noreply.github.com>
2023-07-27 00:49:20 +0530

    Create doublyLinkedList.cpp

---
+++ Linked List/doublyLinkedList.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// structure to define a node of the doubly linked list
+class node{
+    int data;
+    node *prev;
+    node *next;
+
+    node(int data){
+      this-> data = data;
+        
+    }
+};
