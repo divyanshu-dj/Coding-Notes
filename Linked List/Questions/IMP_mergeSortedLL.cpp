49534b9618efd1bcfe25c57016ff9b4ad890b1c3
Divyanshu Kumar Jha <116970264+divyanshu-dj@users.noreply.github.com>
2023-07-29 14:55:37 +0530

    Update and rename mergeSortedLL.cpp to IMP_mergeSortedLL.cpp

--- Linked List/Questions/IMP_mergeSortedLL.cpp
+++ Linked List/Questions/IMP_mergeSortedLL.cpp
@@ -22,33 +22,44 @@
 
 ************************************************************/
 
-void solve(Node<int>* first, Node<int>* second) {
+Nodeint>* solve(Node<int>* first, Node<int>* second) {
+
+    //it only one node in the first linked list 
+    if( first->next == NULL ){
+        first->next = second;
+        return first;
+    }
     
+    Node<int>* curr1 = first;
+    Nodeint>* next1 = curr1 -> next;
     
-    Node* curr1 = first;
-    Node* next1 = curr1 -> next;
-    
-    Node* curr2 = second;
-    Node* next2 = curr2 -> next;
+    Nodeint>* curr2 = second;
+    Nodeint>* next2 = curr2 -> next;
     
     while(next1 != NULL && curr2 != NULL) {
         
         if( (curr2 -> data >= curr1 -> data ) 
            && ( curr2 -> data <= next1 -> data)) {
-            
+
+            //add nodes in between the first list
             curr1 -> next = curr2;
+            next2 = curr2 -> next;
             curr2 -> next = next1;
+            // update pointers
             curr1 = curr2;
             curr2 = next2;
         }
         else {
-            
+            //curr and next ko aage badha rahe hai
+            curr1 = next1;
+            next1 = next1 -> next;
+
+            if( next1 == NULL ){
+                curr1 -> next = curr2;
+            }     
         }
-        
-        
     }
-    
-    
+    return first;
 }
 
 Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
@@ -60,11 +71,11 @@ Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
         return first;
     
     if(first -> data <= second -> data ){
-        solve(first, second);
+        return solve(first, second);
     }
     else
     {
-        solve(second, first);
+        return solve(second, first);
     }
     
     
