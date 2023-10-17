61ce800ad8905cb30198dadcc1d83bca810ae511
Divyanshu Kumar Jha <116970264+divyanshu-dj@users.noreply.github.com>
2023-10-17 13:30:05 +0530

    Create implementation.cpp

---
+++ Tries/implementation.cpp
@@ -0,0 +1,14 @@
+class TrieNode {
+  public:
+    char data;
+    TrieNode* children[26];
+    bool isTerminal;
+
+    TrieNode(char ch) {
+      data = ch;
+      for(int i=0; i<26; i++) {
+        children[i] = NULL;
+      }
+      isTerminal = false;
+    }
+}
