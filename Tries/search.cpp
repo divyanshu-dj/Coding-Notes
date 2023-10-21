0a1fa6ab70fe7634e85f9b942bcf90177e00f84b
Divyanshu Kumar Jha <116970264+divyanshu-dj@users.noreply.github.com>
2023-10-21 17:20:26 +0530

    Update search.cpp

--- Tries/search.cpp
+++ Tries/search.cpp
@@ -1,13 +1,42 @@
 class Trie {
-  
+  public:
+  TrieNode* root;
+
+  Trie() {
+    root = new TrieNode('\0');
+    return;
+  }
 
   bool searchUtil(TrieNode* root, string word) {
     //base case
     if( word.length()==0 ) {
       return root->isTerminal;
     }
+
+    int index = word[0] - 'A';
+    TrieNode* child;
+
+    //present
+    if(root->children[index] != NULL) {
+      child = root->children[index];
+    }
+    else {
+      //absent
+      return false;
+    }
+
+    //recursion
+    return searchUtil(child, word.substr(1));
   }
-  bool search(string word) {
+
+  bool searchWord(string word) {
     return searchUtil(root, word);
   }
 };
+
+
+int main() {
+  Trie* t = new Trie();
+
+  cout<<"1 if present and 0 if not: "<< t->searchWord("abcd")<<endl;
+}
