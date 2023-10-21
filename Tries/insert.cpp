401f42e38faee5374a302a686fdd021473664e2b
Divyanshu Kumar Jha <116970264+divyanshu-dj@users.noreply.github.com>
2023-10-21 17:08:18 +0530

    Update insert.cpp

--- Tries/insert.cpp
+++ Tries/insert.cpp
@@ -36,18 +36,6 @@ class Trie {
   void insertWord(string word) {
     insertUtil(root, word);
   }
-
-
-
-  bool searchUtil(TrieNode* root, string word) {
-    //base case
-    if( word.length()==0 ) {
-      return root->isTerminal;
-    }
-  }
-  bool search(string word) {
-    return searchUtil(root, word);
-  }
 };
 
 int main() {
