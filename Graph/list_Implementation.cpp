c5bfd5361f1038d392d92865a6b3dbb18570d23e
Divyanshu Kumar Jha <116970264+divyanshu-dj@users.noreply.github.com>
2023-10-22 12:24:12 +0530

    Update list_Implementation.cpp

--- Graph/list_Implementation.cpp
+++ Graph/list_Implementation.cpp
@@ -11,3 +11,36 @@ int main() {
   }
   return 0;
 }
+
+
+
+
+///////////////////////////////Approach 2////////////////////////////////////////
+#include<list>
+using namespace std;
+template <typename T>
+
+class graph {
+public:
+  unordered_map<T, list<T> adj;
+
+  void addEdge(T u, T v, bool direction) {
+    //direction = 0 => undirected
+    //direction = 1 => directed graph
+
+    //create an edge from u to v
+    adj[u].push_back(v);
+    if(direction == 0) 
+      adj[v].push_back(u);
+  }
+  void printAdjList(){
+    for(auto i:adj) {
+      cout<< i.first<<"->";
+      for(auto j:i.second) {
+        cout<<j<<",";
+      }
+      cout<<endl;
+    }
+  }
+};
+
