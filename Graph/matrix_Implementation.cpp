3cb913f6a5b511ced19af16561c4e998bdf9094f
Divyanshu Kumar Jha <116970264+divyanshu-dj@users.noreply.github.com>
2023-10-22 10:25:35 +0530

    Update and rename matrixImplementation.cpp to matrix_Implementation.cpp

--- Graph/matrix_Implementation.cpp
+++ Graph/matrix_Implementation.cpp
@@ -2,10 +2,12 @@ int main() {
   int n, m;
   cin>>n>>m;
   int graph[n+1][m+1] = 0;
+  
   for( int i=0, i<m, i++) {
     int u, v;
     cin>>u>>v;
     graph[u][v] = 1;
     graph[v][u] = 1;
   }
+  return 0;
 }
